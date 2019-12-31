90ed22d700f9dde85aea0bd36ebdfa6fda2e49a0
marcos <eptaccio@gmail.com>
2019-12-31 14:56:22 -0300

    add more logs and fail tratatives

--- commands/index.js
+++ commands/index.js
@@ -7,9 +7,32 @@ const configure = bot => {
     hello
   ]
 
-  commands.forEach(({ command, resolve }) => {
-    bot.command(command, ctx => resolve({ ctx, bot }))
-  })
+  for (const { command, resolve, on } of commands) {
+    const genericResolve = async ctx => {
+      const resolving = command || on
+
+      try {
+        console.log(`RESOLVING /${resolving}`)
+        await resolve({ ctx, bot })
+      } catch (error) {
+        console.error(error)
+
+        const id = ctx.message.from.id
+        const text = `fail executing /${resolving}`
+        await bot.telegram.sendMessage(id, text)
+      }
+    }
+
+    if (command) {
+      console.log(`registering command ${command}`)
+      bot.command(command, genericResolve)
+    }
+
+    if (on) {
+      console.log(`registering listener ${on}`)
+      bot.on(on, genericResolve)
+    }
+  }
 }
 
 module.exports = {
