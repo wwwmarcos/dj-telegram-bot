c457febda6ecff3de967435abb159dd4abcd83c4
marcos <eptaccio@gmail.com>
2019-12-31 14:54:56 -0300

    update bot callback

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const config = {
   botToken: process.env.BOT_TOKEN,
-  currentHost: 'https://dj-bot-atletxs.now.sh',
-  isDevelopment: false
+  currentHost: 'https://djproducerbot.now.sh',
+  isDevelopment: process.env.NODE_ENV === 'dev'
 }
 
 module.exports = {
