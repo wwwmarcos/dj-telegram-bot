aa7a8932588d8a5d756037ff74125418f02fb417
marcos <eptaccio@gmail.com>
2020-01-09 00:48:26 -0300

    add actions mapping

--- commands/index.js
+++ commands/index.js
@@ -1,42 +1,56 @@
 const hello = require('./hello')
-const funk = require('./funk')
-const funkActions = require('./funk/actions')
+const funk = require('./music')
+const funkActions = require('./music/actions')
+
+const createResolver = ({ resolving, resolve, bot, info }) => async ctx => {
+  try {
+    console.log(`RESOLVING /${resolving}`)
+    await resolve({ ctx, bot, info })
+  } catch (error) {
+    console.error(error)
+
+    const id = ctx.message.from.id
+    const text = `fail executing /${resolving}`
+    await bot.telegram.sendMessage(id, text)
+  }
+}
 
 const configure = bot => {
   const commands = [
     funk,
     hello,
-    funkActions
+    ...funkActions.actions
   ]
 
-  for (const { command, resolve, on, use } of commands) {
-    const genericResolve = async ctx => {
-      const resolving = command || on
-
-      try {
-        console.log(`RESOLVING /${resolving}`)
-        await resolve({ ctx, bot })
-      } catch (error) {
-        console.error(error)
-
-        const id = ctx.message.from.id
-        const text = `fail executing /${resolving}`
-        await bot.telegram.sendMessage(id, text)
-      }
-    }
-
+  for (const { command, resolve, on, action } of commands) {
     if (command) {
       console.log(`registering command ${command}`)
-      bot.command(command, genericResolve)
+      bot.command(command, createResolver({
+        resolving: command,
+        resolve,
+        bot
+      }))
     }
 
     if (on) {
       console.log(`registering listener ${on}`)
-      bot.on(on, genericResolve)
+      bot.on(on, createResolver({
+        resolving: on,
+        resolve,
+        bot
+      }))
     }
 
-    if (use) {
-      use(bot)
+    if (action) {
+      console.log(`registering action ${action.name}`)
+      bot.action(action.name, createResolver({
+        resolving: action.name,
+        resolve: action.resolve,
+        bot,
+        info: {
+          actionName: action.name
+        }
+      }))
     }
   }
 }
