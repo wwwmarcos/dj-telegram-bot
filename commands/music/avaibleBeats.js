01c4e61f7a61b7338199d30cac42fa6c291957e5
marcos <eptaccio@gmail.com>
2020-01-09 00:48:51 -0300

    add avaible beats list

---
+++ commands/music/avaibleBeats.js
@@ -0,0 +1,14 @@
+const avaibleBeats = [
+  {
+    fileName: 'funk',
+    description: 'FUNKBR'
+  },
+  {
+    fileName: 'rap',
+    description: 'HIP-HOP'
+  }
+]
+
+module.exports = {
+  avaibleBeats
+}
