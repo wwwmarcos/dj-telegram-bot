2b547662045b492c9f57f76d64a0830c6605015e
marcos <eptaccio@gmail.com>
2020-01-12 00:30:30 -0300

    fiz host config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   botToken: process.env.BOT_TOKEN,
-  currentHost: 'https://djproducerbot.eptaccio1.now.sh',
+  currentHost: 'https://djproducerbot.now.sh',
   isDevelopment: process.env.NODE_ENV === 'dev'
 }
 
