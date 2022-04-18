557583e927b2822d4d0230fa5635c4fddc27f579
Marcos Florencio <eptaccio@gmail.com>
2022-04-18 18:50:58 -0300

    use vercel env

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   botToken: process.env.BOT_TOKEN,
-  currentHost: 'https://djproducerbot.now.sh',
+  currentHost: process.env.VERCEL_URL,
   isDevelopment: process.env.NODE_ENV === 'dev'
 }
 
